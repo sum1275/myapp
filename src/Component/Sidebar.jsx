3bfea1e59a20ae5e635757c442a09167947320b1
Sumit Sinha <sumitsinha215@gmail.com>
2022-07-13 11:12:48 +0530

    Idea 1 Login_Panel

---
+++ src/Component/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Nav} from "react-bootstrap";
+import { withRouter } from "react-router";
+import './Sidebar.css'
+
+/*const Side = props =>*/
+function Sidebar(props) {
+   
+
+    return (
+        <>
+    
+            <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
+            activeKey="/home"
+            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+            >
+                <div className="sidebar-sticky"></div>
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                Disabled
+                </Nav.Link>
+            </Nav.Item>
+            </Nav>
+          
+        </>
+        );
+  };
+  //const Sidebar = withRouter(Side);
+  export default Sidebar
